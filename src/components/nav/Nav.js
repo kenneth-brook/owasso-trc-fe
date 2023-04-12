e1990260891abd799cc65176f8ac21c35bbbe274
Kenneth Brook <kenneth.brook@hotmail.com>
2023-04-12 17:11:31 -0400

    nav started

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,8 +1,31 @@
 import React from "react";
+import { Routes, Route, Link } from 'react-router-dom';
+
+import Home from "../home/Home";
+import SponSales from "../sponSales/SponSales";
 
 function Nav() {
     return (
-        <div>NAV</div>
+        <div className="nav">
+            <nav>
+                <ul>
+                    <li>
+                        <Link className="link" to="/" activeClassName="active">
+                            TRC Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className="link" to="/Sponsorship-Sales" activeClassName="active">
+                            Sponsorship Sales
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Sponsorship-Sales" element={<SponSales />} />
+            </Routes>
+        </div>
     )
 }
 
