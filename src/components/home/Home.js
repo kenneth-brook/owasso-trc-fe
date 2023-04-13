b86beeb0792f5ccf2a20040a8ae0eac681630cc8
Kenneth Brook <kenneth.brook@hotmail.com>
2023-04-13 16:39:07 -0400

    goal pannel framing started

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,23 @@
+import therm from '../../images/Group 349.png';
+import prod from '../../images/Group 350.png';
+
 import React from "react";
 
 function Home() {
     return (
-        <div className="homeWrap">
-            <h1>Total Resource Campaign</h1>
-            <p>Obitaspe lendae nobit omnissecta audi ut esti volor mint. Re nem. Ut litibusdam exceatem assima vella consento que lab ium fugitatiatur aute earum volupta spicae optationem. Mendus idusape rnatem.</p>
+        <div>
+            <div className="homeWrap">
+                <h1>Total Resource Campaign</h1>
+                <p>Obitaspe lendae nobit omnissecta audi ut esti volor mint. Re nem. Ut litibusdam exceatem assima vella consento que lab ium fugitatiatur aute earum volupta spicae optationem. Mendus idusape rnatem.</p>
+            </div>
+            <div className="mainBody">
+                <div className='therm'>
+                    <img src={`${therm}`} alt="" />
+                </div>
+                <div className='prods'>
+                    <img src={`${prod}`} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
